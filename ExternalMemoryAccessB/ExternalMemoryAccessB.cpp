edf7ff5966e5c07b1d75f0c5634ee84aeabb8480
Alpha <Alpha@DESKTOP-SIBM4J9>
2023-06-24 08:39:45 +0300

    create

--- ExternalMemoryAccessB/ExternalMemoryAccessB.cpp
+++ ExternalMemoryAccessB/ExternalMemoryAccessB.cpp
@@ -1,21 +1,49 @@
-// ExternalMemoryAccessB.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include "pch.h"
 #include <iostream>
+#include <vector>
+#include <Windows.h>
+#include "proc.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Get ProcId of the target process
+    DWORD procId = GetProcId(L"ac_client.exe");
+
+    //Getmodulebaseaddress
+    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
+
+    //Get Handle to Process
+    HANDLE hProcess = 0;
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+
+    //Resolve base address of the pointer chain
+    uintptr_t dynamicPtrBaseAddr = moduleBase + 0x10f4f4;
+
+    std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
+
+    //Resolve our ammo pointer chain
+    std::vector<unsigned int> ammoOffsets = { 0x374, 0x14, 0x0 };
+    uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
+
+    std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
+
+    //Read Ammo value
+    int ammoValue = 0;
+
+    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+    std::cout << "Current ammo = " << std::dec << ammoValue << std::endl;
+
+    //Write to it
+    int newAmmo = 1337;
+    WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+
+    //Read out again
+    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+
+    std::cout << "New ammo = " << std::dec << ammoValue << std::endl;
+
+    getchar();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
